da513d5500970c81280dd849e711b89511442c68
chh52 <cuongha@gmail.com>
2022-09-20 22:22:05 -0700

    incorporated zillow_api.py code into city_selector.py

--- city_selector.py
+++ city_selector.py
@@ -5,6 +5,8 @@ import hvplot.pandas
 import matplotlib.pyplot as plt
 import plotext as plt2
 from pathlib import Path
+import holoviews as hv
+from zillow_api import zillow_api_request
 
 
 ### Autorun code ###
@@ -16,7 +18,7 @@ def run():
     ### Asks for input from User in CLI environment:
     choice = questionary.select(
         "Which metropolitan city were you considering moving to?",
-        choices=["Atlanta, GA", "Austin, TX", "Boston, MA", "Chicago, IL", "Dallas, TX", "Denver, CO", "Detroit, MI", "Houston, TX", "Las Vegas, NV", "Los Angeles, CA", "Miami, FL", "Minneapolis, MN", "New York City, NY", "Philadelphia, PA", "Phoenix, AZ", "Portland, OR", "Raleigh, NC", "Sacramento, CA", "San Diego, CA", "Seattle, WA", "San Francisco, CA", "Washington, DC", "Quit"]
+        choices=["Atlanta, GA", "Austin, TX", "Boston, MA", "Chicago, IL", "Dallas, TX", "Denver, CO", "Detroit, MI", "Houston, TX", "Las Vegas, NV", "Los Angeles, CA", "Miami, FL", "Minneapolis, MN", "New York City, NY", "Philadelphia, PA", "Phoenix, AZ", "Portland, OR", "Raleigh, NC", "Sacramento, CA", "San Diego, CA", "Seattle, WA", "San Francisco, CA", "Washington, DC"]
     ).ask()
     
     ### Import data from Apartment_List using city selected by User
@@ -29,12 +31,12 @@ def run():
     
     aug_2022_avg = city_df_transposed.values[-1].min()
     
+    # Does request on zillow's API to grab rental listings for city selected above
+    zillow_api_request(city)
+    
     ###
     # Add in calculation to show percentage change from 1 year prior
 
-    ### How do I rename the index??
-    # city_df_transposed.index=['city']
-    # city_df_transposed = city_df_transposed.rename(columns={1: city})
     
     ###Create hvplot and save to .html for user interactivity
     hvplot.save(city_df_transposed.hvplot(
@@ -57,16 +59,36 @@ def run():
     plt.savefig("./Results/city_plot.jpg")
 
     
+    
+
+    
+    #import data from API
+    city_listings_df = pd.read_csv(
+        Path(f'./Resources/{city}.csv')
+    )
+    
+    #city_listings_data = listings_df[listings_df['CITY STATE COLUMN'] == city]
+    #display(city_listings_data.head())
+    
+    #GeoViews plot
+    hv.save(city_listings_df.hvplot.points(
+        'longitude',
+        'latitude',
+        geo=True,
+        tiles='OSM',
+        hover_cols="all",
+        frame_width=500,
+        frame_height=500,
+    ), './Results/geoviews.html')
+    
+
     ### FIX PLOTEXT CODE
-    #plt2.image_plot("./Results/city_plot.jpg")
-    #plt2.title("test")
-    #plt2.show()
-
-    
-
-    
+    # plt2.image_plot("./Results/city_plot.jpg")
+    # plt2.title("test")
+    # plt2.show()
+            
     print(f"\nthe average rent for {city} in August 2022 was {aug_2022_avg}\n")
-    print(f"Please see the following charts in ./Results/ for a view of the average rent history for {city}\n city_plot.jpg\n city_plot.html\n")
+    print(f"Please see the following charts in ./Results/ for a view of the average rent history for {city}\n city_plot.jpg\n city_plot.html\n geoviews.html\n")
     print(f"Thanks for using our Post Pandemic Housing Relocator Tool! Goodbye!")
 
 
---
+++ zillow_api.py
@@ -0,0 +1,49 @@
+import requests
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+def zillow_api_request(city):
+
+    pd.options.display.max_columns = None
+
+    df = pd.read_csv('./api_keys.csv')
+    zwsid = df.loc[df['API'] == 'zillow']['KEY'].iloc[0]
+    rapid_api_key = df.loc[df['API'] == 'rapid']['KEY'].iloc[0]
+
+    url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+    querystring = {"location": city, "status_type":"ForRent", "price":""}
+    headers = {
+        "X-RapidAPI-Key": rapid_api_key,
+        "X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
+    }
+
+    z_rent = requests.request("GET", url, headers=headers, params=querystring)
+    z_rent
+    z_rent.text
+    z_rent_json = z_rent.json()
+    z_rent_json.keys()
+    total_pages = z_rent_json['totalPages']
+    total_pages_list = list(range(1,total_pages + 1))
+    columns=['units', 'listingStatus', 'zpid', 'longitude', 'buildingName','address', 'isBuilding', 'hasImage', 'lotId', 'imgSrc', 'latitude']
+
+
+    z_rent_json = pd.json_normalize(data= z_rent_json['props'])
+    data_df=pd.DataFrame(columns=columns)
+
+    for page in total_pages_list[:20]:
+        querystring = {"location": city, "page":page , "status_type":"ForRent", "price":""}
+        z_rent_json = requests.request("GET", url, headers=headers, params=querystring)
+        z_rent_json = z_rent.json()
+        z_rent_json = pd.json_normalize(z_rent_json['props']).iloc[:,:11]
+        data_df = data_df.append(z_rent_json,ignore_index=True)
+
+    time.sleep(1.0)
+    data_df_expanded = data_df.explode('units').reset_index()
+    unit_info_df = data_df_expanded.reset_index()
+    unit_info_df = unit_info_df.drop(['index'], axis=1)
+    unit_info_df = unit_info_df.drop(['level_0'], axis=1)
+
+    unit_info_df.to_csv(f"./Resources/{city}.csv", index=False)
